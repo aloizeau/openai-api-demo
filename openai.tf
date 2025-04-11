1690347ea2308b948b03ebf5aab271199534694a
Antoine LOIZEAU <antoine.loizeau@hotmail.fr>
2025-04-11 17:11:37 +0200

    Add open ai filters

--- openai.tf
+++ openai.tf
@@ -28,5 +28,124 @@ resource "azurerm_cognitive_deployment" "openai-gpt" {
 
   sku {
     name = "Standard"
+  }  
+}
+
+## Create Content Filter Policy for Azure OpenAI ##
+resource "azurerm_cognitive_account_rai_policy" "content_filter" {
+  name                 = "kopicloud-aoi-cf"
+  cognitive_account_id = azurerm_cognitive_account.openai.id
+  base_policy_name     = "Microsoft.Default"
+  
+  content_filter {
+    name               = "Profanity"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }
+
+  content_filter {
+    name               = "Profanity"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+
+  content_filter {
+    name               = "Hate"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }
+
+  content_filter {
+    name               = "Hate"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+  
+  content_filter {
+    name               = "Violence"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }  
+
+  content_filter {
+    name               = "Violence"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }  
+
+  content_filter {
+    name               = "Sexual"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }  
+
+  content_filter {
+    name               = "Sexual"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+  
+  content_filter {
+    name               = "SelfHarm"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }
+ 
+  content_filter {
+    name               = "SelfHarm"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+
+  content_filter {
+    name               = "Jailbreak"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+
+  content_filter {
+    name               = "Indirect Attack"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Prompt"
+  }
+
+  content_filter {
+    name               = "Protected Material Text"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
+  }
+
+  content_filter {
+    name               = "Protected Material Code"
+    filter_enabled     = true
+    block_enabled      = var.openai_content_filter_block_enabled
+    severity_threshold = var.openai_content_filter_severity_threshold
+    source             = "Completion"
   }
 }
